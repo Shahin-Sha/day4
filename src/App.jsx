a9a822e6775fbd335edd3a1f85919743be00d09c
Shahin-Sha <shahinshakilinadan@gmail.com>
2023-05-25 12:11:17 +0530

    nav test

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import NavBar from './components/NavBar'
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  )
+}
